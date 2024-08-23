2c1f4ccfa819bf16dfa1a8211db44bfba6e6a8fb
sumeet-srknec <sumeet.srknec@gmail.com>
2024-08-23 18:37:38 +0100

    making changes

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
   const { isLoggedOut } = UserAuth();
   return (
     <div>
+      <p className='text-red-100'>Testing 2</p>
       {
         isLoggedOut ? (<NonUserRoutes/>) : (<UserRoutes/>)
       }
